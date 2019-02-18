19be80473cfd6dc7fa9e9eeb2f5bdc7a90d65d21
Daniel Coady <daniel.coady@outlook.com.au>
2019-02-18 14:51:06 +1100

    added array stuffs

---
+++ arrays/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int luckyNums[] = {4, 1, 20, 43};
+    cout << luckyNums[0] << endl;
+    luckyNums[0] = 3;
+    cout << luckyNums[0] << endl;
+    return 0;
+}
